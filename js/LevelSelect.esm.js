93ba2b1ba2f4df1d91b75574c42872e370547e53
Natan Ciupek <nano888@interia.eu>
2021-02-17 19:19:18 +0100

    fix bug

--- js/LevelSelect.esm.js
+++ js/LevelSelect.esm.js
@@ -48,19 +48,19 @@ class LevelSelect extends Common {
 		}
 
 		if (!media.diamondsSprite) {
-			media.diamondsSprite = loader.loadImage('../images/diamonds-transparent.png');
+			media.diamondsSprite = loader.loadImage('images/diamonds-transparent.png');
 		}
 
 		if (!media.backgroundImage) {
-			media.backgroundImage = loader.loadImage('../images/levelbackground.png');
+			media.backgroundImage = loader.loadImage('images/levelbackground.png');
 		}
 
 		if (!media.swapSound) {
-			media.swapSound = loader.loadSound('../sounds/stone_rock_or_wood_moved.mp3');
+			media.swapSound = loader.loadSound('sounds/stone_rock_or_wood_moved.mp3');
 		}
 
 		if (!media.backgroundMusic) {
-			media.backgroundMusic = loader.loadSound('../sounds/music-background.mp3');
+			media.backgroundMusic = loader.loadSound('sounds/music-background.mp3');
 		}
 
 		window.addEventListener(DATALOAED_EVENT_NAME, () => game.playLevel(level));
